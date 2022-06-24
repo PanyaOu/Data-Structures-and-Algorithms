c4044de9220afcd23ed51562e4d478492e2bd7b2
PanyaOu <75552913+PanyaOu@users.noreply.github.com>
2022-06-23 19:34:24 -0700

    Update nQueens.py

--- Recursion/nQueens.py
+++ Recursion/nQueens.py
@@ -1,11 +1,26 @@
 def main():
-    n = 4
+    n = 5
     arr = [["."] * n for x in range(n)]
-    print(NQueens([], arr, 0, n, set(), set(), set(), arr))
+    print(NQueens([], 0, n, set(), set(), set(), arr))
+
+    '''
+    checking specific diagonals
+    (r + c)
+    [0  1  2  3]
+    [1  2  3  4]
+    [2  3  4  5]
+    [3  4  5  6]
+    
+    (r - c)
+    [0 -1 -2 -3]
+    [1  0 -1 -2]
+    [2  1  0 -1]
+    [3  2  1  0]
+    '''
 
 
 # using sets for no duplicates
-def NQueens(result, queens_list, r, n, col, diag1, diag2, arr):
+def NQueens(result, r, n, col, diag1, diag2, arr):
     if r == n:
         copy = ["".join(row) for row in arr]
         return result.append(copy)
@@ -14,17 +29,19 @@ def NQueens(result, queens_list, r, n, col, diag1, diag2, arr):
         if c in col or (r + c) in diag1 or (r - c) in diag2:
             continue
 
-        arr[r][c] = "Q"
         col.add(c)
+        # bottom left to top right: constant values
         diag1.add(r + c)
+        # top to bottom right: constant values
         diag2.add(r - c)
+        arr[r][c] = "Q"
 
-        NQueens(result, queens_list, r + 1, n, col, diag1, diag2, arr)
+        NQueens(result, r + 1, n, col, diag1, diag2, arr)
 
+        col.remove(c)
+        diag1.remove(r + c)
+        diag2.remove(r - c)
         arr[r][c] = "."
-        col.pop()
-        diag1.pop()
-        diag2.pop()
 
     return result
 
